ce9f7f9954b0a985680dee0381dd7aac43ec5fb5
Sergey Bershadsky <929361+sergio-bershadsky@users.noreply.github.com>
2023-06-25 16:14:39 +0100

    fix(initial): manage.pt wrong service

--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -8,7 +8,7 @@ sys.path.append("src")
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dummy.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
